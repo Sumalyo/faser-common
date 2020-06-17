e884119c240227bfcb4cb8481fa193bcc7cabd59
Eric Torrence <eric.torrence@cern.ch>
2020-06-17 13:54:49 -0700

    Fixes for Logging under calypso build

--- EventFormats/EventFormats/DAQFormats.hpp
+++ EventFormats/EventFormats/DAQFormats.hpp
@@ -383,6 +383,7 @@ inline std::ostream &operator<<(std::ostream &out, const  DAQFormats::EventFragm
 inline std::ostream &operator<<(std::ostream &out, const  DAQFormats::EventFull &ev) {
     out<<"Event: "<<std::setw(8)<<ev.event_counter()<<" (0x"<<std::hex<<std::setfill('0') <<std::setw(8)<<ev.event_id()<<") "
        <<std::setfill(' ')
+       <<" run="<<std::dec<<ev.run_number()
        <<" tag="<<std::dec<<static_cast<int>(ev.event_tag())
        <<" bc="<<std::dec<<std::setw(4)<<ev.bc_id()
        <<" trig=0x"<<std::hex<<std::setfill('0')<<std::setw(4)<<ev.trigger_bits()
