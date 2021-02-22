2bcfad21edadf8aa3542c6589bd93ee128dd8123
Savannah Rose Shively <savannah.rose.shively@cern.ch>
2021-02-22 11:04:27 +0000

    Warning debug bool added

--- EventFormats/EventFormats/TrackerDataFragment.hpp
+++ EventFormats/EventFormats/TrackerDataFragment.hpp
@@ -60,7 +60,7 @@ public:
             m_complete = false;
             m_missingData = true;
             m_chipIsValid=false;
-            WARNING("SCTEvent::AddHit :: ERROR: AddHit(): passed chipID is not known! chipID = 0x" + s.str());
+            if (m_debug) {WARNING("SCTEvent::AddHit :: ERROR: AddHit(): passed chipID is not known! chipID = 0x" + s.str());}
           }
           return m_chipIsValid;}
     
