a35bc137b6b88149476806241aa1805ea1ceb9b9
Jens Noerby Kristensen <jens.noerby.kristensen@cern.ch>
2021-07-05 10:05:33 +0200

    ci to use daqling spack deps

--- setup.sh
+++ setup.sh
@@ -9,3 +9,9 @@ export PATH=/opt/ohpc/pub/compiler/gcc/8.3.0/bin:$PATH
 
 export CC='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/gcc'
 export CXX='/opt/ohpc/pub/compiler/gcc/8.3.0/bin/g++'
+
+
+## source spack repos path, to use daqling libraries(ers), if path is supplied:
+if [ $# -eq 1 ]; then
+source $1/spack/share/spack/setup-env.sh
+fi
